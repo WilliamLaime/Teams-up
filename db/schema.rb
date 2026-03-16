09cd98f3671185c0590e71283e169d20e81bb949
William Laime <laimewilliam@gmail.com>
2026-03-16 15:28:39 +0100

    ajout commentaire input rejoindre match

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_16_104908) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_16_142407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_16_104908) do
     t.datetime "created_at", null: false
     t.datetime "last_read_at"
     t.bigint "match_id", null: false
+    t.text "message"
     t.string "role"
     t.string "status", default: "pending"
     t.datetime "updated_at", null: false
